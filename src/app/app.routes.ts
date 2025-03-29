04b919fb269eb0fd61716a3ff210b239bfe861ba
Búzás Barnabás <h255645@stud.u-szeged.hu>
2025-03-29 11:14:08 +0100

    Temporarly removed profile page, added cart page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,6 +2,7 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
 import { ProfileComponent } from './pages/profile/profile.component';
 import { PageNotFoundComponent } from './shared/page-not-found/page-not-found.component';
+import { CartComponent } from './pages/cart/cart.component';
 
 export const routes: Routes = [
     { path: 'home', component: HomeComponent },
@@ -9,6 +10,7 @@ export const routes: Routes = [
         path: 'shop',
         loadComponent: () => import('./pages/shop/shop.component').then(m => m.ShopComponent),
     },
+    { path: 'cart', component: CartComponent },
     { path: 'profile', component: ProfileComponent },
     { path: '', redirectTo: 'home', pathMatch: 'full' },
     { path: '**', component: PageNotFoundComponent },
